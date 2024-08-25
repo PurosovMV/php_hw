a2c0c7282b8b6a30940fdbb01a09e02611c16cac
PurosovMV <mihail.purosov@gmail.com>
2024-08-25 20:50:08 +0300

    add hw1

--- hw1/php-cli/start.php
+++ hw1/php-cli/start.php
@@ -1,10 +1,16 @@
 <?php
-$a = 5;
-$b = '05';
-var_dump($a == $b);
-var_dump((int)'012345');
-var_dump((float)123.0 === (int)123.0);
-var_dump(0 == 'hello, world');
+$a = 1;
+$b = 2;
+
+// Вариант №1.
+//$a = $a + $b;
+//$b = $a - $b;
+//$a = $a - $b;
+
+// Вариант №2.
+$a = $a ^ $b ^ $b = $a;
+
+echo "a = {$a} b = {$b}";
 
 
-// docker run --rm -v ${pwd}/php-cli/:/cli php:8.2-cli php /cli/start.php
+// docker run --rm -v $(pwd)/php-cli/:/cli php:8.2-cli php /cli/start.php
